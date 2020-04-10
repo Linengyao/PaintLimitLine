3b5852a4da3c6db6debaab8bf2fdc40acef900d0
Linengyao <1547784788@qq.com>
2020-04-10 15:13:29 +0800

    2020.4.10第三题

--- PaintLimitLine/PaintLimitLine.cpp
+++ PaintLimitLine/PaintLimitLine.cpp
@@ -1,11 +1,43 @@
-﻿// PaintLimitLine.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// PaintStraightLine.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
+#include <opencv.hpp>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cv::Mat dstMat;
+	cv::Mat	srcMat = cv::imread("E:\\课程\\大二下\\数字图像处理\\20200410.png", 0);
+	cv::Mat CannyMat;
+	Canny(srcMat, CannyMat, 150, 200);
+
+	//cv::Mat lineMat;
+	//cv::HoughLines(CannyMat, lineMat,1, CV_PI / 180,2);
+
+
+
+	std::vector<cv::Vec4i>	lines;
+	cv::HoughLinesP(CannyMat, lines, 1, CV_PI / 180, 30,5,10);
+	std::vector<cv::Vec4i>::iterator it = lines.begin();
+	
+	//绘制直线参考代码
+	for (; it != lines.end(); ++it)
+	{
+		cv::Point pt1, pt2;
+		pt1.x = (*it)[0];
+		pt1.y = (*it)[1];
+		pt2.x = (*it)[2];
+		pt2.y = (*it)[3];
+		cv::line(srcMat, pt1, pt2, cv::Scalar(255, 0, 0), 1, CV_AA);
+	}
+
+
+	imshow("srcMat", srcMat);
+	imshow("CanntMat", CannyMat);
+	waitKey(0);
+	//std::cout << "Hello World!\n";
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
